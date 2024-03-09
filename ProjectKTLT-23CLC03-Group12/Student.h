79a2e9799ff111954d168b7742ba81967eecb297
giabao1509 <162421305+giabao1509@users.noreply.github.com>
2024-03-10 00:37:40 +0700

    Them file struct Student
    Add Student.h

---
+++ ProjectKTLT-23CLC03-Group12/Student.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include "Date.h"
+
+struct student
+{
+    int No;
+    std::string studentID, firstName, lastName, gender, socialID;
+    date dateOfBirth;
+    struct scoreBoard
+    {
+        double totalMark;
+        double finalMark;
+        double midtermMark;
+        double otherMark;
+        scoreBoard* pNext;
+    };
+    student* pStudent;
+    student* pNext;
+    //constructor
+    student(int no, std::string id, std::string firstname, std::string lastname, std::string gender, date dob, std::string socialId) :
+		No(no), studentID(id), firstName(firstname), lastName(lastname), gender(gender),
+		dateOfBirth(dob), socialID(socialId){}
+};
