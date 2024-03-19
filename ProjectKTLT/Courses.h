674c99cb60477ad5b531f919fec6ec541288335c
RainyinSaiGon <dinhdaivu1109@gmail.com>
2024-03-20 00:55:47 +0700

    Add Course.h

--- ProjectKTLT/Courses.h
+++ ProjectKTLT/Courses.h
@@ -4,10 +4,97 @@
 #include <string>
 #include <fstream>
 
-struct StudentScore
+struct Course
 {
-	std::string StudentID;
+
+	//Student score in the course
+	struct studentScore
+	{
+		std::string studentID;
+		std::string firstName;
+		std::string lastName;
+		double totalMark, finalMark, midtermMark, otherMark;
+		studentScore* pNext;
+	};
+
+	// Student basic infomation
+	std::string studentID, name, className, lecturer, weekDay;
+
+	// Credit, session
+	int credit, maxStudent, session;
+
+	//construstor
+	Course(std::string studentID, std::string name, std::string className, std::string lecturer, int credit, int maxStudent, std::string weekDay, int session);
 	
+	//ponter to score total
+	studentScore* pHeadScore = nullptr; 
+
+	//pointer to student in course
+	Course* pNext = nullptr;
+	student* pHeadStudent = nullptr;
+	student* pTailStudent = nullptr;
+
+
+
+	// Function
+	void updateCourse();
+	void addStudentMenu();
+	void addStudent(int choice);
+	void deleteStudent();
+	void showStudent();
+	void showInfo();
+	void deleteStudentCourseList();
+	void deleteCourseList();
+
+};
+
+#pragma once
+
+#include "Student.h"
+#include <string>
+#include <fstream>
+
+struct Course
+{
+
+	//Student score in the course
+	struct studentScore
+	{
+		std::string studentID;
+		std::string firstName;
+		std::string lastName;
+		double totalMark, finalMark, midtermMark, otherMark;
+		studentScore* pNext;
+	};
+
+	// Student basic infomation
+	std::string studentID, name, className, lecturer, weekDay;
+
+	// Credit, session
+	int credit, maxStudent, session;
+
+	//construstor
+	Course(std::string studentID, std::string name, std::string className, std::string lecturer, int credit, int maxStudent, std::string weekDay, int session);
+	
+	//ponter to score total
+	studentScore* pHeadScore = nullptr; 
+
+	//pointer to student in course
+	Course* pNext = nullptr;
+	student* pHeadStudent = nullptr;
+	student* pTailStudent = nullptr;
+
+
+
+	// Function
+	void updateCourse();
+	void addStudentMenu();
+	void addStudent(int choice);
+	void deleteStudent();
+	void showStudent();
+	void showInfo();
+	void deleteStudentCourseList();
+	void deleteCourseList();
 
 };
 
