adc58c1b595568e9d0bbc8ec94313ee1aac6e134
KidCute1412 <23127404@student.hcmus.edu.vn>
2024-04-13 23:41:48 +0700

    dunno

--- ProjectKTLT/ProjectKTLT.cpp
+++ ProjectKTLT/ProjectKTLT.cpp
@@ -9,7 +9,30 @@
 
 // Functions
 int main() {
+	/*Static* a = new Static;
+	student* curStudent = new student;
+	semester* tmp = new semester;
+	int n = tmp->specifyCourseForStudent(a);
+	
+	std::string **check = new std::string*[n];
+	check = curStudent->getAllCoursesInformations(a);
 
+	for (int i = 0; i < n; i++)
+	{
+		
+		for (int j = 0; j < 8; j++)
+		{
+			std::cout << check[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	delete tmp;
+	delete a;
+	delete curStudent;
+	delete[] check;
+	return 0;*/
+
+	
     Scene* scene = new Scene;
     sf::RenderWindow window(sf::VideoMode((unsigned int)scene->a->width, (unsigned int)scene->a->height), "CourseManagingSystem");
 
